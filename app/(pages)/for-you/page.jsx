3900d288095875fc6e64e4124460ecc14b15d724
overkill489 <overkill.alt.489@gmail.com>
2026-09-19 13:29:03 -0500

    for-you-page

--- app/(pages)/for-you/page.jsx
+++ app/(pages)/for-you/page.jsx
@@ -1,4 +1,18 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function ForYou() {
+  const [book, setBook] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      "https://us-central1-summaristt.cloudfunctions.net/getBooks?status=selected",
+    )
+      .then((res) => res.json())
+      .then((data) => setBook(data))
+  }, []);
+
   return (
     <div className="max-w-5xl w-full mx-auto py-6">
       <div className="px-10 w-full">
@@ -10,13 +24,11 @@ export default function ForYou() {
             href=""
             className="flex justify-between w-2/3 bg-[#fbefd6] rounded-md p-6 mb-6 gap-6"
           >
-            <div className="text-[#032b41] w-2/5">
-              How Constant Innovation Creates Radically Successful Businesses
-            </div>
-            <div className="w-0.5 bg-[#bac8ce]"/>
+            <div className="text-[#032b41] w-2/5">{book.subTitle}</div>
+            <div className="w-0.5 bg-[#bac8ce]" />
             <div className="flex gap-4 w-7/12">
               <figure className="h-32 w-32 min-w-32">
-                <img src=".../assets/TheLeanStartup" alt="" className="w-full h-full" />
+                <img src="/assets/" alt="" className="w-full h-full" />
               </figure>
             </div>
           </a>
